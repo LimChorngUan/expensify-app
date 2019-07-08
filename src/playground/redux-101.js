a5596e58919e923fc5d38a62b5ad8870a88232cf
Lim Chorng Uan <limchornguan418@gmail.com>
2019-07-08 21:56:41 +0200

    add store and actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,4 +1,56 @@
 import React from 'react'
+import { createStore } from 'redux'
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case `INCREMENT`:
+      return { count: state.count + action.incrementBy }
+    case `DECREMENT`:
+      return { count: state.count - action.decrementBy }
+    case `SET`:
+      return { count: action.setCount }
+    case `RESET`:
+      return { count: 0 }
+    default:
+      return state
+  }
+})
+
+const addIncrement = ({ incrementBy = 1 } = {}) => ({
+  type: `INCREMENT`,
+  incrementBy,
+})
+
+const addDecrement = ({ decrementBy = 1 } = {}) => ({
+  type: `DECREMENT`,
+  decrementBy,
+})
+
+const addSetCount = ({ setCount }) => ({
+  type: `SET`,
+  setCount,
+})
+
+const addReset = () => ({
+  type: `RESET`,
+})
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(addIncrement())
+
+store.dispatch(addIncrement({ incrementBy: 5 }))
+
+store.dispatch(addSetCount({ setCount: 101 }))
+
+store.dispatch(addReset())
+
+store.dispatch(addDecrement())
+
+store.dispatch(addDecrement({ decrementBy: 10 }))
+
 
 const ReduxPlayground = () => (
   <div>
